828148f524ba3e4242caa75b7489ee11bca45946
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2022-04-04 21:18:48 +0300

    Test crash

--- MoppApp/MoppApp/SigningViewController.swift
+++ MoppApp/MoppApp/SigningViewController.swift
@@ -48,6 +48,17 @@ class SigningViewController : MoppViewController {
         importButton.accessibilityLabel = L(.signatureViewBeginLabel)
         
         UIAccessibility.post(notification: .screenChanged, argument: importButton)
+        
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Test Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        let numbers = [0]
+        let _ = numbers[1]
     }
     
     override func viewWillDisappear(_ animated: Bool) {
